2eb5318c9743cd40980ea6398b0af343988a6403
Bishnu Mishra <bishnumishra01996@gmail.com>
2022-05-28 06:09:33 +0530

    add no_of_good_pair.js

---
+++ no_of_good_pair.js
@@ -0,0 +1,13 @@
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+   for(let i = 0;i<nums.length-1;i++){
+       for(let j = i + 1;j<=nums.length-1;j++){
+           if(nums[i] === nums[j]){
+               count++;
+           }
+       }
+   }
+   return count;
+};
+
+console.log(numIdenticalPairs([1,2,3,1,1,3]));
